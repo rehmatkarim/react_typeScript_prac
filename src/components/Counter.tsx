d9a4e972070b81cc2e5b27aab905d251d01280f6
rehmatkarim <p146120@nu.edu.pk>
2021-03-22 16:29:50 -0400

    added condition on counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -8,6 +8,7 @@ const Counter: React.VoidFunctionComponent<countType> = ({ count }) => {
   const [value, setValue] = useState(count);
   return (
     <div>
+        {value>=10 && alert('number is too large')}
       <p>{value}</p>
       <button onClick={(e) => setValue(value + 1)}>Increment</button>
       <button onClick={(e) => setValue(value - 1)}>Decrement</button>
